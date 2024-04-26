f4d54a0ddb00d2008ff7126f5d18e4a6c996b1bb
Shubham Tejani <64226852+shubham404-error@users.noreply.github.com>
2024-04-26 17:16:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ diabetes_model = pickle.load(open("./models/diabetes_model_new.sav",'rb'))
 heart_model = pickle.load(open("./models/heart_disease_model.sav",'rb'))
 parkinsons_model = pickle.load(open("./models/parkinsons_model.sav",'rb'))
 breast_model = pickle.load(open("./models/breast_cancer_model.sav",'rb'))
+kidney_disease_model = pickle.load(open(f'./models/kidney.pkl','rb'))
+
 
 # sidebar navigation
 with st.sidebar:
@@ -19,8 +21,9 @@ with st.sidebar:
                            ['Heart Disease Prediction',
                             'Diabetes Prediction',
                             'Parkinson\'s Prediction',
-                            'Breast Cancer Prediction'],
-                           icons=['heart','activity','person','gender-female'],
+                            'Breast Cancer Prediction','Kidney Disease Prediction'],
+                           menu_icon='hospital-fill',
+                           icons=['heart','activity','brain','gender-female','person'],
                            default_index=0)
     
 
@@ -351,6 +354,107 @@ if selected == 'Breast Cancer Prediction':
                 cancer_diagnosis = 'The person is not diagnosed with breast cancer.'
         
     st.success(cancer_diagnosis)
+    
+    if selected == 'Kidney Disease Prediction':
+    
+    st.title("Kidney Disease Prediction using ML")
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    with col1:
+        age = st.text_input('Age')
+
+    with col2:
+        blood_pressure = st.text_input('Blood Pressure')
+
+    with col3:
+        specific_gravity = st.text_input('Specific Gravity')
+
+    with col4:
+        albumin = st.text_input('Albumin')
+
+    with col5:
+        sugar = st.text_input('Sugar')
+
+    with col1:
+        red_blood_cells = st.text_input('Red Blood Cell')
+
+    with col2:
+        pus_cell = st.text_input('Pus Cell')
+
+    with col3:
+        pus_cell_clumps = st.text_input('Pus Cell Clumps')
+
+    with col4:
+        bacteria = st.text_input('Bacteria')
+
+    with col5:
+        blood_glucose_random = st.text_input('Blood Glucose Random')
+
+    with col1:
+        blood_urea = st.text_input('Blood Urea')
+
+    with col2:
+        serum_creatinine = st.text_input('Serum Creatinine')
+
+    with col3:
+        sodium = st.text_input('Sodium')
+
+    with col4:
+        potassium = st.text_input('Potassium')
+
+    with col5:
+        haemoglobin = st.text_input('Haemoglobin')
+
+    with col1:
+        packed_cell_volume = st.text_input('Packet Cell Volume')
+
+    with col2:
+        white_blood_cell_count = st.text_input('White Blood Cell Count')
+
+    with col3:
+        red_blood_cell_count = st.text_input('Red Blood Cell Count')
+
+    with col4:
+        hypertension = st.text_input('Hypertension')
+
+    with col5:
+        diabetes_mellitus = st.text_input('Diabetes Mellitus')
+
+    with col1:
+        coronary_artery_disease = st.text_input('Coronary Artery Disease')
+
+    with col2:
+        appetite = st.text_input('Appetitte')
+
+    with col3:
+        peda_edema = st.text_input('Peda Edema')
+    with col4:
+        aanemia = st.text_input('Aanemia')
+
+    # code for Prediction
+    kindey_diagnosis = ''
+
+    # creating a button for Prediction    
+    if st.button("Kidney's Test Result"):
+
+        user_input = [age, blood_pressure, specific_gravity, albumin, sugar,
+       red_blood_cells, pus_cell, pus_cell_clumps, bacteria,
+       blood_glucose_random, blood_urea, serum_creatinine, sodium,
+       potassium, haemoglobin, packed_cell_volume,
+       white_blood_cell_count, red_blood_cell_count, hypertension,
+       diabetes_mellitus, coronary_artery_disease, appetite,
+       peda_edema, aanemia]
+
+        user_input = [float(x) for x in user_input]
+
+        prediction = kidney_disease_model.predict([user_input])
+
+        if prediction[0] == 1:
+            kindey_diagnosis = "The person has Kidney's disease"
+        else:
+            kindey_diagnosis = "The person does not have Kidney's disease"
+    st.success(kindey_diagnosis)
 # Add a footer to the app
 footer="""<style>
 a:link , a:visited{
